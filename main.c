0b7f04ac7f9fb0c2708e21e4abbed6e2ff1dabbb
Andrey <amdev88@gmail.com>
2025-08-26 23:39:13 -0300

    remove test code

--- main.c
+++ main.c
@@ -6,21 +6,9 @@ int main() {
     initFila(&fila);
     initPilha(&pilha);
     srand(time(NULL));
-    char codigos[] = {'I', 'L', 'T', 'O', 'I'};
-    int ids[] = {0, 1, 2, 3, 4};
-    for(int i = 0; i < 5; i++) {
-        Peca peca;
-        peca.tipo = codigos[i];
-        peca.id = ids[i];
+    for(int i = 0; i < QUEUE_SIZE; i++) {
+        Peca peca = geraPeca();
         enqueue(&fila, peca);
-    }
-    char codigosPilha[] = {'T', 'L', 'O'};
-    int idsPilha[] = {6, 7, 8};
-    for(int i = 0; i < 3; i++) {
-        Peca peca;
-        peca.tipo = codigosPilha[i];
-        peca.id = idsPilha[i];
-        push(&pilha, peca);
     }
     int opcao = -1;
     printEstado(&fila, &pilha);
